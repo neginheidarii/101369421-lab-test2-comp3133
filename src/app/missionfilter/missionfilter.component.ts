df627148cf2d604a2ca200d2e0e6591d38353f67
Negin <113136968+neginheidarii@users.noreply.github.com>
2024-04-04 00:32:59 -0400

    added functionalities

--- src/app/missionfilter/missionfilter.component.ts
+++ src/app/missionfilter/missionfilter.component.ts
@@ -1,37 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Missionservice } from '../network/missionservice.service';
-import { Mission } from '../models/mission';
-import { ActivatedRoute } from '@angular/router';
-
-
+import { Component, OnInit } from "@angular/core";
+import { Missionservice } from "../network/missionservice.service";
+import { Mission } from "../models/mission";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-missionfilter',
-  templateUrl: './missionfilter.component.html',
-  styleUrls: ['./missionfilter.component.css']
+  selector: "app-missionfilter",
+  templateUrl: "./missionfilter.component.html",
+  styleUrls: ["./missionfilter.component.css"],
 })
-export class MissionfilterComponent  {
+export class MissionfilterComponent {
   missions?: Mission[];
   years?: string[];
 
-  constructor(private route: ActivatedRoute, private missionService: Missionservice) {}
+  constructor(
+    private route: ActivatedRoute,
+    private missionService: Missionservice
+  ) {}
 
   ngOnInit() {
-    this.route.queryParamMap.subscribe(params => {
-      const year = params.get('year');
+    this.route.queryParamMap.subscribe((params) => {
+      const year = params.get("year");
       if (year) {
-        this.missionService.getMissionsByYear(year).subscribe(missions => {
+        this.missionService.getMissionsByYear(year).subscribe((missions) => {
           this.missions = missions;
         });
       } else {
-        this.missionService.getMissions().subscribe(missions => {
+        this.missionService.getMissions().subscribe((missions) => {
           this.missions = missions;
         });
       }
     });
 
-    this.missionService.getMissions().subscribe(missions => {
-      this.years = Array.from(new Set(missions.map(m => m.launch_year)));
+    this.missionService.getMissions().subscribe((missions) => {
+      this.years = Array.from(new Set(missions.map((m) => m.launch_year)));
     });
   }
 }
